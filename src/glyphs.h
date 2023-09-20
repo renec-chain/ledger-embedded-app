170c7ad5a77ce7d1d1b478d30fbe74f88d673f76
dungtrinh97 <dungtrinh@remitano.com>
2023-09-20 18:47:58 +0700

    Fix Stax build

--- src/glyphs.h
+++ src/glyphs.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #ifndef GLYPH_icon_back_BPP
 #define GLYPH_icon_back_WIDTH  14
 #define GLYPH_icon_back_HEIGHT 14
@@ -7,8 +9,10 @@ extern unsigned char const C_icon_back_bitmap[25];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_back;
-#endif  // GLYPH_icon_back_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_back_BPP
+#pragma once
+
 #ifndef GLYPH_icon_back_x_BPP
 #define GLYPH_icon_back_x_WIDTH  14
 #define GLYPH_icon_back_x_HEIGHT 14
@@ -18,8 +22,10 @@ extern unsigned char const C_icon_back_x_bitmap[25];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_back_x;
-#endif  // GLYPH_icon_back_x_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_back_x_BPP
+#pragma once
+
 #ifndef GLYPH_icon_certificate_BPP
 #define GLYPH_icon_certificate_WIDTH  14
 #define GLYPH_icon_certificate_HEIGHT 14
@@ -29,8 +35,10 @@ extern unsigned char const C_icon_certificate_bitmap[25];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_certificate;
-#endif  // GLYPH_icon_certificate_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_certificate_BPP
+#pragma once
+
 #ifndef GLYPH_icon_coggle_BPP
 #define GLYPH_icon_coggle_WIDTH  14
 #define GLYPH_icon_coggle_HEIGHT 14
@@ -40,8 +48,10 @@ extern unsigned char const C_icon_coggle_bitmap[25];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_coggle;
-#endif  // GLYPH_icon_coggle_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_coggle_BPP
+#pragma once
+
 #ifndef GLYPH_icon_crossmark_BPP
 #define GLYPH_icon_crossmark_WIDTH  14
 #define GLYPH_icon_crossmark_HEIGHT 14
@@ -51,8 +61,10 @@ extern unsigned char const C_icon_crossmark_bitmap[25];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_crossmark;
-#endif  // GLYPH_icon_crossmark_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_crossmark_BPP
+#pragma once
+
 #ifndef GLYPH_icon_dashboard_BPP
 #define GLYPH_icon_dashboard_WIDTH  14
 #define GLYPH_icon_dashboard_HEIGHT 14
@@ -62,8 +74,10 @@ extern unsigned char const C_icon_dashboard_bitmap[25];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_dashboard;
-#endif  // GLYPH_icon_dashboard_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_dashboard_BPP
+#pragma once
+
 #ifndef GLYPH_icon_dashboard_x_BPP
 #define GLYPH_icon_dashboard_x_WIDTH  14
 #define GLYPH_icon_dashboard_x_HEIGHT 14
@@ -73,8 +87,10 @@ extern unsigned char const C_icon_dashboard_x_bitmap[25];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_dashboard_x;
-#endif  // GLYPH_icon_dashboard_x_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_dashboard_x_BPP
+#pragma once
+
 #ifndef GLYPH_icon_down_BPP
 #define GLYPH_icon_down_WIDTH  7
 #define GLYPH_icon_down_HEIGHT 4
@@ -84,8 +100,10 @@ extern unsigned char const C_icon_down_bitmap[4];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_down;
-#endif  // GLYPH_icon_down_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_down_BPP
+#pragma once
+
 #ifndef GLYPH_icon_eye_BPP
 #define GLYPH_icon_eye_WIDTH  14
 #define GLYPH_icon_eye_HEIGHT 14
@@ -95,8 +113,10 @@ extern unsigned char const C_icon_eye_bitmap[25];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_eye;
-#endif  // GLYPH_icon_eye_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_eye_BPP
+#pragma once
+
 #ifndef GLYPH_icon_left_BPP
 #define GLYPH_icon_left_WIDTH  4
 #define GLYPH_icon_left_HEIGHT 7
@@ -106,8 +126,10 @@ extern unsigned char const C_icon_left_bitmap[4];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_left;
-#endif  // GLYPH_icon_left_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_left_BPP
+#pragma once
+
 #ifndef GLYPH_icon_right_BPP
 #define GLYPH_icon_right_WIDTH  4
 #define GLYPH_icon_right_HEIGHT 7
@@ -117,8 +139,10 @@ extern unsigned char const C_icon_right_bitmap[4];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_right;
-#endif  // GLYPH_icon_right_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_right_BPP
+#pragma once
+
 #ifndef GLYPH_icon_renec_16x16_BPP
 #define GLYPH_icon_renec_16x16_WIDTH  16
 #define GLYPH_icon_renec_16x16_HEIGHT 16
@@ -128,19 +152,23 @@ extern unsigned char const C_icon_renec_16x16_bitmap[32];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_renec_16x16;
-#endif  // GLYPH_icon_renec_16x16_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_renec_16x16_BPP
+#pragma once
+
 #ifndef GLYPH_icon_renec_64x64_BPP
 #define GLYPH_icon_renec_64x64_WIDTH  64
 #define GLYPH_icon_renec_64x64_HEIGHT 64
-#define GLYPH_icon_renec_64x64_BPP    1
+#define GLYPH_icon_renec_64x64_BPP    4
 extern unsigned int const C_icon_renec_64x64_colors[];
-extern unsigned char const C_icon_renec_64x64_bitmap[512];
+extern unsigned char const C_icon_renec_64x64_bitmap[2048];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_renec_64x64;
-#endif  // GLYPH_icon_renec_64x64_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_renec_64x64_BPP
+#pragma once
+
 #ifndef GLYPH_icon_up_BPP
 #define GLYPH_icon_up_WIDTH  7
 #define GLYPH_icon_up_HEIGHT 4
@@ -150,8 +178,10 @@ extern unsigned char const C_icon_up_bitmap[4];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_up;
-#endif  // GLYPH_icon_up_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_up_BPP
+#pragma once
+
 #ifndef GLYPH_icon_validate_14_BPP
 #define GLYPH_icon_validate_14_WIDTH  14
 #define GLYPH_icon_validate_14_HEIGHT 14
@@ -161,8 +191,10 @@ extern unsigned char const C_icon_validate_14_bitmap[25];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_validate_14;
-#endif  // GLYPH_icon_validate_14_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_validate_14_BPP
+#pragma once
+
 #ifndef GLYPH_icon_warning_BPP
 #define GLYPH_icon_warning_WIDTH  14
 #define GLYPH_icon_warning_HEIGHT 14
@@ -172,5 +204,18 @@ extern unsigned char const C_icon_warning_bitmap[25];
 #ifdef HAVE_BAGL
 #include "bagl.h"
 extern const bagl_icon_details_t C_icon_warning;
-#endif  // GLYPH_icon_warning_BPP
 #endif  // HAVE_BAGL
+#endif  // GLYPH_icon_warning_BPP
+#pragma once
+
+#ifndef GLYPH_icon_processing_BPP
+#define GLYPH_icon_processing_WIDTH  14
+#define GLYPH_icon_processing_HEIGHT 14
+#define GLYPH_icon_processing_BPP    1
+extern unsigned int const C_icon_processing_colors[];
+extern unsigned char const C_icon_processing_bitmap[25];
+#ifdef HAVE_BAGL
+#include "bagl.h"
+extern const bagl_icon_details_t C_icon_processing;
+#endif  // HAVE_BAGL
+#endif  // GLYPH_icon_processing_BPP
